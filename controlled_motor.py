73895c4e392fd5e636107801de61f5ad6d0b2bc7
Bozhinov <marogo142005@gmail.com>
2023-04-03 20:51:11 +0300

    [ADD] Pololu Driver support

--- controlled_motor.py
+++ controlled_motor.py
@@ -1,41 +1,36 @@
-from RPi import GPIO
-from time import sleep
+import time
+from dual_g2_hpmd_rpi import motors, MAX_SPEED
 
-in1 = 24
-in2 = 23
-en = 25
-temp1 = 1
+# Define a custom exception to raise if a fault is detected.
+class DriverFault(Exception):
+    def __init__(self, driver_num):
+        self.driver_num = driver_num
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(en, GPIO.OUT)
-GPIO.output(in1, GPIO.LOW)
-GPIO.output(in2, GPIO.LOW)
-
-p = GPIO.PWM(en, 1000)
-p.start(25)
+def raiseIfFault():
+    if motors.motor1.getFault():
+        raise DriverFault(1)
+    if motors.motor2.getFault():
+        raise DriverFault(2)
 
 try:
+    motors.setSpeeds(0, 0)
     while True:
-        inp = input("Enter speed: ")
+        inp = input("Enter speed for Motor 1: ")
         if inp == "q":
             break
-        speed = int(inp)
-        if speed > 0:
-            GPIO.output(in1, GPIO.HIGH)
-            GPIO.output(in2, GPIO.LOW)
-            print("forward")
-            p.ChangeDutyCycle(speed)
-        elif speed < 0:
-            GPIO.output(in1, GPIO.LOW)
-            GPIO.output(in2, GPIO.HIGH)
-            print("backward")
-            p.ChangeDutyCycle(-speed)
-        else:
-            GPIO.output(in1, GPIO.LOW)
-            GPIO.output(in2, GPIO.LOW)
-            print("stop")
-            p.ChangeDutyCycle(0)
+        speed1 = int(inp)
+        inp = input("Enter speed for Motor 2: ")
+        if inp == "q":
+            break
+        speed2 = int(inp)
+        motors.setSpeeds(speed1, speed2)
+        raiseIfFault()
+        time.sleep(0.002)
+
+except DriverFault as e:
+    print("Driver %s fault!" % e.driver_num)
+
 finally:
-    GPIO.cleanup()
+    # Stop the motors, even if there is an exception
+    # or the user presses Ctrl+C to kill the process.
+    motors.forceStop()
--- controlled_motor_by_encoder.py
+++ controlled_motor_by_encoder.py
@@ -1,60 +1,72 @@
+import time
+from dual_g2_hpmd_rpi import motors, MAX_SPEED
 from RPi import GPIO
-from time import sleep
 
-in1 = 24
-in2 = 23
-en = 25
-temp1 = 1
-
-encoder_clk = 17
-encoder_data = 18
+encoder_clk_1 = 17
+encoder_data_1 = 18
+encoder_clk_2 = 27
+encoder_data_2 = 22
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(en, GPIO.OUT)
-GPIO.setup(encoder_clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(encoder_data, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.output(in1, GPIO.LOW)
-GPIO.output(in2, GPIO.LOW)
+GPIO.setup(encoder_clk_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(encoder_data_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(encoder_clk_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(encoder_data_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-p = GPIO.PWM(en, 1000)
-p.start(25)
+# Define a custom exception to raise if a fault is detected.
+class DriverFault(Exception):
+    def __init__(self, driver_num):
+        self.driver_num = driver_num
 
-val = 25
+def raiseIfFault():
+    if motors.motor1.getFault():
+        raise DriverFault(1)
+    if motors.motor2.getFault():
+        raise DriverFault(2)
 
-step_size = 5
+val = MAX_SPEED / 4 # start at 25% speed
+ 
+step_size = MAX_SPEED / 120 # 4/480 speed per step
 
-clkLastState = GPIO.input(encoder_clk)
+clkLastState_1 = GPIO.input(encoder_clk_1)
+clkLastState_2 = GPIO.input(encoder_clk_2)
 
 try:
     while True:
-        # use the rotary encoder to control the speed
-        clkState = GPIO.input(encoder_clk)
-        dtState = GPIO.input(encoder_data)
-        if clkState != clkLastState:
-            if dtState != clkState:
+        clkState_1 = GPIO.input(encoder_clk_1)
+        dtState_1 = GPIO.input(encoder_data_1)
+        clkState_2 = GPIO.input(encoder_clk_2)
+        dtState_2 = GPIO.input(encoder_data_2)
+        if clkState_1 != clkLastState_1:
+            if dtState_1 != clkState_1:
                 val = val + step_size
             else:
                 val = val - step_size
-            print(val)
-            if val > 100:
-                val = 100
-            if val > 0:
-                GPIO.output(in1, GPIO.HIGH)
-                GPIO.output(in2, GPIO.LOW)
-                print("forward")
-                p.ChangeDutyCycle(val)
-            elif val < 0:
-                GPIO.output(in1, GPIO.LOW)
-                GPIO.output(in2, GPIO.HIGH)
-                print("backward")
-                p.ChangeDutyCycle(-val)
+            print('Motor 1: ' + str(val))
+            if val > MAX_SPEED:
+                val = MAX_SPEED
+            motors.setSpeeds(val, val)
+            raiseIfFault()
+            time.sleep(0.002)
+        clkLastState_1 = clkState_1
+        if clkState_2 != clkLastState_2:
+            if dtState_2 != clkState_2:
+                val = val + step_size
             else:
-                GPIO.output(in1, GPIO.LOW)
-                GPIO.output(in2, GPIO.LOW)
-                print("stop")
-        clkLastState = clkState
-        sleep(0.01)
+                val = val - step_size
+            print('Motor 2: ' + str(val))
+            if val > MAX_SPEED:
+                val = MAX_SPEED
+            motors.setSpeeds(val, val)
+            raiseIfFault()
+            time.sleep(0.002)
+        clkLastState_2 = clkState_2
+        time.sleep(0.01)
+
+
+except DriverFault as e:
+    print("Driver %s fault!" % e.driver_num)
+
 finally:
+    motors.forceStop()
     GPIO.cleanup()
