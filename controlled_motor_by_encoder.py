bcc4f5b3ff8e7e41c384cb53b9dbc68b96e8005b
School_Bozhinov <marogo142005@gmail.com>
2023-04-08 13:38:11 +0300

    [FIX] motor speeds

--- controlled_motor_by_encoder.py
+++ controlled_motor_by_encoder.py
@@ -4,15 +4,11 @@ from RPi import GPIO
 
 encoder_clk_1 = 17
 encoder_data_1 = 18
-encoder_clk_2 = 27
-encoder_data_2 = 22
-button = 23
+button = 27
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(encoder_clk_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(encoder_data_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(encoder_clk_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(encoder_data_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Define a custom exception to raise if a fault is detected.
@@ -27,12 +23,10 @@ def raiseIfFault():
         raise DriverFault(2)
 
 val_1 = MAX_SPEED / 2 # start at 50% speed
-val_2 = MAX_SPEED / 2 # start at 50% speed
  
 step_size = MAX_SPEED / 30 # 16 speed per step
 
 clkLastState_1 = GPIO.input(encoder_clk_1)
-clkLastState_2 = GPIO.input(encoder_clk_2)
 
 button_pressed = False
 
@@ -40,8 +34,6 @@ try:
     while not button_pressed:
         clkState_1 = GPIO.input(encoder_clk_1)
         dtState_1 = GPIO.input(encoder_data_1)
-        clkState_2 = GPIO.input(encoder_clk_2)
-        dtState_2 = GPIO.input(encoder_data_2)
         if clkState_1 != clkLastState_1:
             if dtState_1 != clkState_1:
                 val_1 = val_1 + step_size
@@ -52,24 +44,10 @@ try:
                 val_1 = MAX_SPEED
             if val_1 <= 0:
                 val_1 = 0
-            motors.motor1.setSpeed(-val_1)
+            motors.setSpeeds(-val_1, val_1)
             raiseIfFault()
             time.sleep(0.002)
         clkLastState_1 = clkState_1
-        if clkState_2 != clkLastState_2:
-            if dtState_2 != clkState_2:
-                val_2 = val_2 + step_size
-            else:
-                val_2 = val_2 - step_size
-            print('Motor 2: ' + str(val_2))
-            if val_2 >= MAX_SPEED:
-                val_2 = MAX_SPEED
-            if val_1 <= 0:
-                val_1 = 0
-            motors.motor2.setSpeed(val_2)
-            raiseIfFault()
-            time.sleep(0.002)
-        clkLastState_2 = clkState_2
 
         if GPIO.input(button) == GPIO.LOW:
             button_pressed = True
@@ -78,14 +56,14 @@ try:
         time.sleep(0.01)
     else:
         # Stop the motors slowly.
-        while val_1 > 0 or val_2 > 0:
+        while val_1 > 0 or val_1 > 0:
             val_1 = val_1 - step_size
-            val_2 = val_2 - step_size
+            val_1 = val_1 - step_size
             if val_1 < 0:
                 val_1 = 0
-            if val_2 < 0:
-                val_2 = 0
-            motors.setSpeeds(val_1, val_2)
+            if val_1 < 0:
+                val_1 = 0
+            motors.setSpeeds(-val_1, val_1)
             raiseIfFault()
             time.sleep(0.01)
 
